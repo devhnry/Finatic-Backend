2762523b1a43a6adc1a1ef92d36b2c8ea4a92ceb
devhnry <devthenry@gmail.com>
2025-01-13 13:56:34 +0100

    Initial Commit

---
+++ src/main/java/dev/finaticbackend/FinaticBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.finaticbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaticBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinaticBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/finaticbackend/FinaticBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.finaticbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaticBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
